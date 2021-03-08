2f84d769c374251a7d150b64777591c8243680c8
abepuentes <48102930+abepuentes@users.noreply.github.com>
2021-03-08 19:25:50 -0300

    connect Cognito user with Database user

--- App.js
+++ App.js
@@ -6,17 +6,63 @@
  * @flow strict-local
  */
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useEffect} from 'react';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import {SafeAreaView, StyleSheet, StatusBar} from 'react-native';
 
 import Navigation from './src/navigation';
 
-import {withAuthenticator} from 'aws-amplify-react-native';
-
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
+import {Auth, API, graphOperation, graphqlOperation} from 'aws-amplify';
+import {withAuthenticator} from 'aws-amplify-react-native';
+import {createUser} from './src/graphql/mutations';
+import {getUser} from './src/graphql/queries';
+
+const randomImages = [
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-2.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-3.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-6.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-9.jpg',
+];
+
+const getRandomImage = () => {
+  return randomImages[Math.floor(Math.random() + randomImages.length)];
+};
+
 function App() {
+  useEffect(() => {
+    const fetchUser = async () => {
+      // get currently authenticated user
+      const userInfo = await Auth.currentAuthenticatedUser({bypassCache: true});
+      if (!userInfo) {
+        return;
+      }
+
+      // check if the user exist in database
+      const me = await API.graphql(
+        graphqlOperation(getUser, {id: userInfo.attributes.sub}),
+      );
+      if (me.data.getUser) {
+        console.log('User already exists in database');
+        return;
+      }
+
+      // if it doesn't (it's newly registered user)
+      // then, create a new use in database
+      const newUser = {
+        id: userInfo.attributes.sub,
+        username: userInfo.username,
+        email: userInfo.attributes.email,
+        imageUri: getRandomImage(),
+      };
+
+      await API.graphql(graphqlOperation(createUser, {input: newUser}));
+    };
+
+    fetchUser();
+  }, []);
+
   return (
     <>
       <StatusBar barStyle="light-content" />
